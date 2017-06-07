62caeb54891b88b252fe78bd4120ff43a45063d4
Anton Gorshkov <antong@gmail.com>
2017-06-06 22:37:47 -0400

    various updates

--- flask_app.py
+++ flask_app.py
@@ -3,6 +3,7 @@ from twilio.twiml.messaging_response import MessagingResponse, Message
 from twilio.rest import Client
 from datetime import date
 from datetime import timedelta
+import json
 
 # Your Account SID from twilio.com/console
 account_sid = "<SECRET>"
@@ -12,20 +13,85 @@ client = Client(account_sid, auth_token)
 # set the project root directory as the static folder, you can set others.
 app = Flask(__name__, static_url_path='')
 
+def hasNumbers(inputString):
+    return any(char.isdigit() for char in inputString)
+
+def get_messages():
+    today = date.today()
+    tomorrow = today + timedelta(days=1)
+    yesterday = today - timedelta(days=1)
+    all_messages = client.messages.list(date_sent=today) + client.messages.list(date_sent=tomorrow) + client.messages.list(date_sent=yesterday)
+    return all_messages
+
+def get_subs():
+    res = []
+    all_messages = get_messages()
+    for message in reversed(all_messages):
+        if message.direction == 'inbound' and message.body.lower() == 'subscribe':
+            res.append(message.from_)
+    return res
+
+def send_message(msg):
+    message = client.messages.create(
+        to="+FROM",
+        from_="+TO",
+        body=msg)
+
 @app.route('/')
 def index():
     return redirect("/html/index.html", code=302)
 
+@app.route('/total')
+def total():
+    res = []
+    all_messages = get_messages()
+    for message in reversed(all_messages):
+        if message.direction == 'inbound' and message.body.lower() != 'subscribe':
+            res.append(sum([int(s) for s in message.body.split() if s.isdigit()]))
+    msg = {'total': sum(res)}
+    return jsonify(msg)
+
+@app.route('/messages_details')
+def messages_details():
+    res = []
+    all_messages = get_messages()
+    for message in all_messages:
+        if message.direction == 'inbound':
+            o = {   'sid': message.sid,
+                    'body': message.body,
+                    'from_': message.from_,
+                    'date_sent': message.date_sent
+            };
+            res.append(o)
+
+    msg = {'messages': res }
+    return jsonify(msg)
+
+@app.route('/message_delete')
+def message_delete():
+    sid = request.args.get('sid', '')
+    client.messages(sid).delete()
+    return 'OK'
+
 @app.route('/messages')
 def hello_world():
     res = []
+    tot_res = []
     today = date.today()
     tomorrow = today + timedelta(days=1)
     all_messages = client.messages.list(date_sent=today) + client.messages.list(date_sent=tomorrow)
     for message in reversed(all_messages):
-        if message.direction == 'inbound':
+        if message.direction == 'inbound' and message.body.lower() != 'subscribe':
             res.append(message.body)
-    msg = {'messages': res}
+            tot_res.append(sum([int(s) for s in message.body.split() if s.isdigit()]))
+
+    msg = {'messages': res, 'total': sum(tot_res) }
+    return jsonify(msg)
+
+@app.route('/subs')
+def subscribers():
+    res = get_subs()
+    msg = {'subs': res}
     return jsonify(msg)
 
 @app.route('/js/<path:path>')
@@ -40,9 +106,16 @@ def send_html(path):
 def hello_monkey():
     from_number = request.values.get('From', None)
     body = request.values.get('Body', None)
+    send_message(str(from_number)+':'+body)
+    if hasNumbers(body):
+        response_text = "Thank you for supporting the JCH on it's 90th Anniversary. You can complete your pledge here: http://www.jchb.org/donate/#online or a staff will contact you to fulfill your pledge"
+    else:
+        response_text = "Thank you for supporting the JCH on it's 90th Anniversary."
+
+#    if body.lower() == 'subscribe':
+#        response_text = "Thank you for subscribing, you'll be receiving updates throughout the event. Text STOP to unsubscribe."
     resp = MessagingResponse()
-    msg = Message().body("Hello, Mobile Monkey 2!")
-    #.media("/html/jch-90th-header-new.jpg")
+    msg = Message().body(response_text).media("/html/jch-90th-header-new.jpg")
     resp.append(msg)
     return str(resp)
 
